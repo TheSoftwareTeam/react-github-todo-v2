231160076c57c0a837d5cf59ca629d2dfeae1e0c
mahirkursun <mahirkrsn@gmail.com>
2022-11-01 23:48:28 +0300

    master

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
-
 const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
 const bodyParser = require("body-parser");
 
-const host = "localhost";
+const path = require("path");
+
 
 const client_id = "cb9bb57da585db57597f";
 const client_secret = "4e43b91fecea1daee467627a5c80d687b3ffb89c";
@@ -15,7 +14,6 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname + "/public")));
 
 app.get("/getAccessToken", async function (req, res) {
   console.log(req.query.code);
@@ -60,6 +58,8 @@ app.get("/getUserData", async function (req, res) {
     });
 });
 
-app.listen(4000, host, function () {
+
+app.listen(4000,'0.0.0.0', function () {
   console.log("CORS server running on port 4000");
 });
+app.use(express.static(path.join(__dirname+"/public")))
